c998e6af75327465aca3b39038cab0b8bb222c12
Rafael Nogueira Rabelo <rafaelnrabelo@gmail.com>
2021-09-13 21:12:06 -0300

    Segunda Parte Pronta

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
-import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi";
+import { useSession, signIn } from "next-auth/client";
+
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 import styles from "./styles.module.scss";
 
@@ -7,9 +9,38 @@ interface SubscribeButtonProps {
   priceId: string;
 }
 
+interface SubscribeAPIResponse {
+  sessionId: string;
+}
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post<SubscribeAPIResponse>("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripeJs = await getStripeJs();
+
+      await stripeJs?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app";
+import { Provider, getSession } from "next-auth/client";
 import { Header } from "../components/Header";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 export default MyApp;
